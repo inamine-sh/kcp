fcb16bf9490a63e9315e309ef8e18fd0b68360a7
iTAP <iTAP@iTAP-PC10>
2016-05-25 09:55:10 +0900

    Loginを行うファイル作成

---
+++ app/controllers/LoginController.java
@@ -0,0 +1,13 @@
+package controllers;
+
+import play.mvc.*;
+
+public class LoginController extends Controller {
+    public Result index() {
+        return TODO;
+    }
+
+    public Result authenticate() {
+        return TODO;
+    }
+}
