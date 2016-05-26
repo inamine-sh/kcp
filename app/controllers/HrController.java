726508c87d8a568d50da28b2eaafa7591cb23e21
iTAP <iTAP@iTAP-PC10>
2016-05-26 15:35:50 +0900

    人事専用ページの作成

--- app/controllers/HrController.java
+++ app/controllers/HrController.java
@@ -1,6 +1,14 @@
 package controllers;
 
 
+import java.util.List;
+import java.util.Map;
+
+import com.avaje.ebean.ExpressionList;
+
+import models.Card;
+import models.Category;
+import models.User;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.hr;
@@ -8,6 +16,22 @@ import views.html.hr;
 public class HrController extends Controller {
 
     public Result index() {
-        return ok(hr.render());
+
+        ExpressionList<Card> temp = Card.find.where();
+        temp = temp.eq("grade", null);
+
+        List <Card> card = temp.findList();
+
+        List<User> user = User.find.all();
+        List<Category> category = Category.find.all();
+
+
+        return ok(hr.render(card,user,category));
+
     }
-}
+
+ }
+
+
+
+
