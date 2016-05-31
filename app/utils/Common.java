d3144283a26ad073a58d13f9d45239caa5b77925
SPL-031 <SPL-031@SPL-031-PC>
2016-05-31 10:02:01 +0900

    preparedParamsメソッドを作成

---
+++ app/utils/Common.java
@@ -0,0 +1,20 @@
+package utils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Common {
+
+    public static Map<String, String> preparedParams(Map<String, String[]> getParams) {
+        Map<String, String> params = new HashMap<>();
+
+        for(Map.Entry<String, String[]> getParam: getParams.entrySet()) {
+            if(getParam != null) {
+                params.put(getParam.getKey(), getParam.getValue()[0]);
+            }
+        }
+
+        return params;
+    }
+
+}
